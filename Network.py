88bfba61afbf38121f8e4e3683f2b049a51a3b68
hellvetica42 <petar.slavicek@gmail.com>
2020-05-12 12:00:59 +0200

    IT WORKS on an xor function the mistake i spent about 10 hours on is that averaging the error of a neuron over a batch of elements doesnt make sense for an xor problem bc the errors cancel eachother out batch size of 1 works here although the network needs a few attempts to find the right valley

---
+++ Network.py
@@ -0,0 +1,129 @@
+import numpy as np
+from sigmoid import *
+import random
+
+
+class Network(object):
+
+    def __init__(self, shapes):
+        """shape is a list of sizes of layers
+        So shape [2, 3, 2] would have
+        3 layers
+        2 inputs, 2 outputs ans 3 hidden neurons"""
+
+        self.numLayers = len(shapes)
+        self.shapes = shapes
+        self.biases = [np.random.randn(x, 1) for x in shapes[1:]]
+        self.weights = [np.random.randn(y, x) for x,y in zip(shapes[:-1], shapes[1:])]
+
+    def feedForward(self, activation):
+        #return output of network
+        #iterate over all layers and feed activation forward
+
+        activation = np.array(activation)
+        for b, w in zip(self.biases, self.weights):
+           activation = sigmoid(np.matmul(w, activation) + b[0])
+        return activation
+
+
+    def StochasticGradientDescent(self, trainingData, epochs, batchSize, learningRate):
+
+
+        n = len(trainingData)
+
+        for e in range(epochs):
+            random.shuffle(trainingData) 
+            mini_batches = [trainingData[m:m+batchSize] for m in range(0, n, batchSize)]
+            epochCost = 0
+            for batch in mini_batches:
+                epochCost += self.runBatch(batch, learningRate)
+            
+            print("Epoch %i finished with Cost: " % e, epochCost)
+
+
+    def runBatch(self, batch, learningRate):
+
+        b_gradient = [np.zeros(b.shape) for b in self.biases]
+        w_gradient = [np.zeros(w.shape) for w in self.weights]
+
+        batchCost = 0
+
+        for b in batch:
+            w_d_gradient, b_d_gradient, totalCost = self.backPropagation(b[0], b[1])
+            # w_d_gradient, b_d_gradient, totalCost = self.backPropagation(np.array([0]), np.array([0]))
+
+            batchCost += totalCost
+
+            #add up the errors over the whole batch to use as gradient
+            w_gradient = [wg+wdg for wg, wdg in zip(w_gradient, w_d_gradient)]
+            b_gradient = [bg+bdg for bg, bdg in zip(b_gradient, b_d_gradient)]
+
+        #adjust weights and biases
+        self.weights = [w - (learningRate/len(batch)) * w_g
+                        for w, w_g in zip(self.weights, w_gradient)]
+
+        self.biases = [b - (learningRate/len(batch)) * w_b
+                        for b, w_b in zip(self.biases, b_gradient)]
+
+        return batchCost 
+        
+
+    
+    def backPropagation(self, x, y):
+        #return touple of lists of arrays/vectors of cost function partial derivatives to weights/biases 
+
+
+        y = np.array(y)[np.newaxis].T 
+
+
+        b_gradient = [np.zeros(b.shape) for b in self.biases]
+        w_gradient = [np.zeros(w.shape) for w in self.weights]
+
+        activation = np.array(x)[np.newaxis].T #first layer activation and used for iteration
+        # activation = np.array(x)
+        activations = [activation] #activation layer by layer 
+        weightedSums = [] #list of all weighted sums layer by layer
+
+        for b, w in zip(self.biases, self.weights):
+            zs = np.matmul(w, activation) + b[0]
+            weightedSums.append(zs)
+            activation = sigmoid(zs)
+            activations.append(activation)
+        
+        #get errors of last layer with cost function
+
+        error = self.d_cost(activations[-1], y) * dsigmoid(weightedSums[-1])
+        error = error
+        b_gradient[-1] = error
+
+        totalCost = np.absolute(error)
+
+
+        w_gradient[-1] = np.matmul(error, activations[-2].T)
+        #the newaxis thing already transposes the vector, dont ask me why
+
+
+        #propagate error backwards through network
+
+        for l in range(2, self.numLayers):
+
+            #find error in current layer
+            error = np.matmul(self.weights[-l+1].transpose(), error) * dsigmoid(weightedSums[-l])
+            #error = error[np.newaxis]
+
+            b_gradient[-l] = error
+
+
+            w_gradient[-l] = np.matmul(error, np.array(activations[-l-1].transpose()))
+            #w_gradient[-l] = (w_gradient[-l])[np.newaxis]
+
+        return (w_gradient, b_gradient, totalCost)
+
+
+
+
+    def d_cost(self, outputActivation, data):
+        return (outputActivation - data) 
+        
+
+
---
+++ xor.py
@@ -0,0 +1,48 @@
+from Network import *
+import numpy as np
+import random
+
+xor = [
+    (np.array([0, 0]), np.array([0])),
+    (np.array([1, 0]), np.array([1])),
+    (np.array([0, 1]), np.array([1])),
+    (np.array([1, 1]), np.array([0])),
+]
+
+training_data = []
+
+for i in range(1000):
+    training_data.append(xor[random.randint(0,3)])
+
+# print(training_data[0], training_data[0][0].shape, training_data[0][0].shape)
+learningRate = 0.1
+
+N = Network([2,2,1])
+N.StochasticGradientDescent(training_data, 100, 1, 0.3)
+
+for x in xor:
+    print(x)
+    print(N.feedForward(x[0]))
+
+# for i in range(500):
+#     for j in range(100):
+#         x = xor[random.randint(0,3)]
+
+#         b_gradient = [np.zeros(b.shape) for b in N.biases]
+#         w_gradient = [np.zeros(w.shape) for w in N.weights]
+
+#         w_d_gradient , b_d_gradient, err = N.backPropagation(x[0], x[1])
+
+#         w_gradient = [wg+wdg for wg, wdg in zip(w_gradient, w_d_gradient)]
+#         b_gradient = [bg+bdg for bg, bdg in zip(b_gradient, b_d_gradient)]
+
+#     N.weights = [w - (learningRate) * w_g
+#                     for w, w_g in zip(N.weights, w_gradient)]
+
+#     N.biases = [b - (learningRate) * w_b
+#                     for b, w_b in zip(N.biases, b_gradient)]
+                    
+
+# for x in xor:
+#     print(x)
+#     print(N.feedForward(x[0]))
